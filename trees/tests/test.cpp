429485439fc0878dd22d200752dd9c2c7e6a28b0
frankogaz <fogaz@miuandes.cl>
2022-11-01 21:46:06 -0300

    funciona

--- trees/tests/test.cpp
+++ trees/tests/test.cpp
@@ -2,17 +2,33 @@
 #include "trees/tree.hpp"
 #include <cstring>
 
-int main(int nargs, char** vargs){
-	//aqui va un while, y exit lo termina
+using namespace std;
+//aqui va la funcion 
 
-	trees::Tree treeSO;//aqui se crea arbol inial
-	treeSO.setRoot(new trees::TreeNode(10));
-	treeSO.insert(5,10);
-	treeSO.traverse();
-	std::cout<<"Mostrar los hijos de 10" << std::endl;
-	trees::TreeNode* node = treeSO.find(10);
-	if (node != nullptr){
-		node->getChildren()->print();
+int main(int nargs, char** vargs){
+	bool ciclo=false;
+	cout<<"$TreeSO"<< endl<<"¡Bienvenido a TreeSO!"<< endl <<"¡Autores: <estudiante1> y <estudiante2>!"<<endl;
+	while (ciclo==false){
+		int estado=0;//estado es actualidad del programa
+		cout<<"/#"<<estado<<endl;
+		cout<<"Que quiere hacer?"<<endl;
+		if (vargs=="cd"}{
+
+		}
+
+
+
+
+		trees::Tree treeSO;//aqui se crea arbol inial
+		treeSO.setRoot(new trees::TreeNode(10));
+		treeSO.insert(5,10);
+		treeSO.traverse();
+		std::cout<<"Mostrar los hijos de 10" << std::endl;
+		trees::TreeNode* node = treeSO.find(10);
+		if (node != nullptr){
+			node->getChildren()->print();
+		}
+		if 
 	}
 	return 0;
 }
